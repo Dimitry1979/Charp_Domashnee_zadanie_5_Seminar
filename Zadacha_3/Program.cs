d5a5b0c2e981f7c730d95674a47c0b76fa1dfbeb
Dmitry <semiashkin@tut.by>
2023-12-11 08:23:20 +0300

    Решили задачу

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет 
+// находить строку с наименьшей суммой элементов.
+
+using System;
+
+namespace FindMinSumRowInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаём двумерный массив
+            int[,] array = new int[3, 4];
+
+            // Заполняем массив случайными числами
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(100);
+                }
+            }
+
+            // Выводим оригинальный массив
+                Console.WriteLine("Оригинальный массив:");
+                
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write("{0,2} ", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            // Находим строку с наименьшей суммой элементов
+            int minSumRow = 0;
+            int minSum = int.MaxValue;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    sum += array[i, j];
+                }
+                if (sum < minSum)
+                {
+                    minSum = sum;
+                    minSumRow = i;
+                }
+            }
+
+            // Выводим строку с наименьшей суммой элементов
+            Console.WriteLine("Строка с наименьшей суммой элементов:");
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write("{0,2} ", array[minSumRow, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
